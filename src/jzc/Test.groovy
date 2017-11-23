69fa2d06c6d3d12f6d02a13cca5b32af1dbf5689
jinzhicai <1943967653@qq.com>
2017-11-23 14:30:37 +0800

    daweqdf

--- src/jzc/Test.groovy
+++ src/jzc/Test.groovy
@@ -12,17 +12,17 @@ class Test {
 //		println var
 //		println var.class
 		
-//		def va =12334
-//		println va
-//		println va.class
-//		def var ="""hello 
-//                 Groovy
-//                 i love you"""
-//		println var
-//		println var.class
-//		var=123
-//		println var
-//		println var.class
+		def va =12334
+		println va
+		println va.class
+		def var ="""hello 
+                 Groovy
+                 i love you"""
+		println var
+		println var.class
+		var=123
+		println var
+		println var.class
 		
 //		def repeat(val,repeat=3){
 //			for(i in 0..<repeat){
